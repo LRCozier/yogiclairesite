703fc58eeccf14ee4d874ee1fc56152a1a3730c8
LRCozier <lrcozier@gmail.com>
2025-02-06 06:59:41 +0000

    imported react-social-icons

--- yoga/src/Components/Footer/Footer.jsx
+++ yoga/src/Components/Footer/Footer.jsx
@@ -1,12 +1,16 @@
 import "./Footer.css";
+import {SocialIcon} from 'react-social-icons';
+import 'react-social-icons/facebook';
+import 'react-social-icons/instagram';
+
 
 const Footer = () => {
   return (
     <footer>
       <p>&copy; 2025 Yogi Claire. All rights reserved.</p>
       <div id="yogiclaire-footer-socials">
-        <a href="https://www.facebook.com/yogabogaclaire" className="fa fa-facebook"></a>
-        <a href="https://www.instagram.com/yogiclairem_/" className="fa fa-instagram"></a>
+        <SocialIcon url="https://www.facebook.com/yogabogaclaire"/>
+        <SocialIcon url="https://www.instagram.com/yogiclairem_/"/>
       </div>
     </footer>
   );
