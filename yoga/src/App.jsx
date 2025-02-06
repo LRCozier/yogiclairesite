094713503226c15c3b6ac5ea19120b4961c1ea46
LRCozier <lrcozier@gmail.com>
2025-02-06 06:55:27 +0000

    composed app folder

--- yoga/src/App.jsx
+++ yoga/src/App.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from './Components/Navbar';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Classes from './Pages/Classes';
+import Contact from './Pages/Contact';
 import './App.css'
 
-function App() {
+const App = () => {
 
   return (
     <>
+    <Router>
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/about' element={<About/>} />
+          <Route path='/classes' element={<Classes/>} />
+          <Route path='/contact' element={<Contact/>} />
+        </Routes>
+      </main>
+      <Footer />
+    </Router>
     </>
   )
 }
