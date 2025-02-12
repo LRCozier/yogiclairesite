bdd911fba799d217fd04cf708a73580de88f79d3
LRCozier <lrcozier@gmail.com>
2025-02-12 12:14:50 +0000

    added Helmet to about page

--- yoga/src/Pages/About.jsx
+++ yoga/src/Pages/About.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import {helmet} from "react-helmet-async";
+import {Helmet} from "react-helmet-async";
 
 const About = () => {
   return (
-    <helmet>
+    <Helmet>
       <tilte>About Yogi Claire</tilte>
       <meta name="description" content="Learn about Yogi Claire, a dedicated yoga instructor in Richmond upon Thames." />
     <section id="about" className="yogiclaire-section">
       <h2 className="yogiclaire-section-title">About Yogi Claire</h2>
       <p>Yoga is more than just physical postures; it's a holistic practice that connects body, mind, and spirit.</p>
     </section>
-    </helmet>
+    </Helmet>
   );
 };
 
