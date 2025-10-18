b8d90d296b570a6f7877367a0095838f7e38128d
LRCozier <lrcozier@gmail.com>
2025-10-18 16:29:40 +0100

    Reworked footer styles to scss

--- yoga/src/Components/Footer/Footer.jsx
+++ yoga/src/Components/Footer/Footer.jsx
@@ -3,44 +3,35 @@ import { Link } from 'react-router-dom';
 import { SocialIcon } from 'react-social-icons';
 import 'react-social-icons/facebook';
 import 'react-social-icons/instagram';
-import './Footer.css';
+import './Footer.scss';
 
 const Footer = () => {
   return (
-    <footer className="site-footer">
-      <div className="container footer-content">
-        <div className="footer-about">
+    <footer className="footer">
+      <div className="container footer__content">
+        <div className="footer__about">
           <h3>Yogi Claire</h3>
           <p>Find your balance in Richmond upon Thames.</p>
         </div>
-        <div className="footer-links">
+        <div className="footer__links">
           <h4>Quick Links</h4>
           <ul>
             <li><Link to="/about">About</Link></li>
             <li><Link to="/classes">Classes</Link></li>
+            <li><Link to="/retreats">Retreats</Link></li>
             <li><Link to="/contact">Contact</Link></li>
           </ul>
         </div>
-        <div className="footer-socials">
+        <div className="footer__socials">
           <h4>Connect</h4>
-            <div className="social-icons-wrapper">
-                <SocialIcon 
-                    url="https://www.facebook.com/" 
-                    bgColor="transparent" 
-                    fgColor="var(--color-primary)" 
-                    style={{ height: 40, width: 40 }}
-                />
-                <SocialIcon 
-                    url="https://www.instagram.com/" 
-                    bgColor="transparent" 
-                    fgColor="var(--color-primary)" 
-                    style={{ height: 40, width: 40 }}
-                />
-            </div>
+          <div className="social-icons">
+            <SocialIcon url="https://www.facebook.com/" bgColor="transparent" fgColor="var(--color-primary)" style={{ height: 40, width: 40 }} />
+            <SocialIcon url="https://www.instagram.com/" bgColor="transparent" fgColor="var(--color-primary)" style={{ height: 40, width: 40 }} />
+          </div>
         </div>
       </div>
-      <div className="footer-bottom">
-        <p>&copy; {new Date().getFullYear()} Claire Marismari. All rights reserved.</p>
+      <div className="footer__bottom">
+        <p>&copy; {new Date().getFullYear()} Yogi Claire. All rights reserved.</p>
         <p>Built by Luke Rudderham-Cozier</p>
       </div>
     </footer>
